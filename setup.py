4a2cc297319272cb5154c5fba6794c107c1845cc
Ruairi-Osul <rorylikesmusic@gmail.com>
2022-06-10 20:01:49 -0400

    added requirements to install

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    reqs = f.read().splitlines()
+
 setup(
     name="drn_analysis",
     version="0.0.1",
@@ -7,4 +10,5 @@ setup(
     author_email="ruairi.osullivan.work@gmail.ie",
     packages=find_packages(),
     python_requires=">=3.6",
+    install_requires=reqs,
 )
