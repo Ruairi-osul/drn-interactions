fdff6ead541c02cfa18139eca52ced112246dee2
Ruairi-osul <rorylikesmusic@gmail.com>
2022-07-18 10:04:39 -0400

    plots grid maker

--- drn_interactions/spiketrains/plots.py
+++ drn_interactions/spiketrains/plots.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Dict, Optional, Sequence, Tuple
+from typing import Dict, Optional, Sequence, Tuple, Any, List
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -8,18 +8,26 @@ from drn_interactions.plots import heatmap
 from sklearn.preprocessing import robust_scale
 from drn_interactions.config import Config
 from drn_interactions.io import load_waveforms
+from drn_interactions.transforms import SpikesHandler
 from drn_interactions.spiketrains.waveforms import WaveformPreprocessor
+import matplotlib as mpl
 
 
 class NeuronTypesFigureLoader:
     def __init__(
         self,
+        example_session_name: str,
+        example_session_t_start: float,
+        example_session_length: float = 10,
         probe_neuron_types_path: Optional[Path] = None,
         probe_waveform_props_path: Optional[Path] = None,
         probe_spiketrain_props_path: Optional[Path] = None,
         single_unit_props_path: Optional[Path] = None,
         single_unit_neuron_types_path: Optional[Path] = None,
     ):
+        self.example_session_name = example_session_name
+        self.example_session_t_start = example_session_t_start
+        self.example_session_length = example_session_length
         self.probe_neuron_types_path = (
             probe_neuron_types_path
             if probe_neuron_types_path is not None
@@ -46,7 +54,35 @@ class NeuronTypesFigureLoader:
             else Config.derived_data_dir / "neuron_types_single_unit.csv"
         )
 
-    def __call__(
+    def load_neuron_types(self) -> pd.DataFrame:
+        return pd.read_csv(self.probe_neuron_types_path)
+
+    def load_waveforms(self) -> pd.DataFrame:
+        return load_waveforms()
+
+    def load_waveform_props(self) -> pd.DataFrame:
+        return pd.read_csv(self.probe_waveform_props_path)
+
+    def load_spiketrain_props(self) -> pd.DataFrame:
+        return pd.read_csv(self.probe_spiketrain_props_path)
+
+    def load_single_unit_props(self) -> pd.DataFrame:
+        return pd.read_csv(self.single_unit_props_path)
+
+    def load_single_unit_neuron_types(self) -> pd.DataFrame:
+        return pd.read_csv(self.single_unit_neuron_types_path)
+
+    def load_spikes(self) -> pd.DataFrame:
+        sh = SpikesHandler(
+            block="pre",
+            session_names=[self.example_session_name],
+            bin_width=10,
+            t_start=self.example_session_t_start,
+            t_stop=self.example_session_t_start + self.example_session_length,
+        )
+        return sh.spikes
+
+    def load_all(
         self,
     ) -> Tuple[
         pd.DataFrame,
@@ -55,13 +91,15 @@ class NeuronTypesFigureLoader:
         pd.DataFrame,
         pd.DataFrame,
         pd.DataFrame,
+        pd.DataFrame,
     ]:
-        probe_neuron_types = pd.read_csv(self.probe_neuron_types_path)
-        probe_waveforms = load_waveforms()
-        probe_waveform_props = pd.read_csv(self.probe_waveform_props_path)
-        probe_spiketrain_props = pd.read_csv(self.probe_spiketrain_props_path)
-        single_unit_props = pd.read_csv(self.single_unit_props_path)
-        single_unit_neuron_types = pd.read_csv(self.single_unit_neuron_types_path)
+        probe_neuron_types = self.load_neuron_types()
+        probe_waveforms = self.load_waveforms()
+        probe_waveform_props = self.load_waveform_props()
+        probe_spiketrain_props = self.load_spiketrain_props()
+        single_unit_props = self.load_single_unit_props()
+        single_unit_neuron_types = self.load_single_unit_neuron_types()
+        spikes = self.load_spikes()
         return (
             probe_neuron_types,
             probe_waveforms,
@@ -69,6 +107,7 @@ class NeuronTypesFigureLoader:
             probe_spiketrain_props,
             single_unit_props,
             single_unit_neuron_types,
+            spikes,
         )
 
 
@@ -77,6 +116,10 @@ class NeuronTypesFigurePreprocessor:
         self,
         electrode_type_order: Sequence[str] = ("Silicon Probe", "Glass Pipette"),
         neuron_type_order: Sequence[str] = ("SR", "SIR", "FF"),
+        spikes_id_col: str = "neuron_id",
+        spikes_spiketime_col: str = "spiketimes",
+        min_spikes_per_train: int = 2,
+        max_spikes_per_train: int = 150,
         new_col_names: Sequence[str] = (
             "ID",
             "Neuron Type",
@@ -114,6 +157,10 @@ class NeuronTypesFigurePreprocessor:
         neuron_type_col: str = "Neuron Type",
         waveform_preprocessor: Optional[WaveformPreprocessor] = None,
     ):
+        self.min_spikes_per_train = min_spikes_per_train
+        self.max_spikes_per_train = max_spikes_per_train
+        self.spikes_id_col = spikes_id_col
+        self.spikes_spiketime_col = spikes_spiketime_col
         self.electrode_type_order = electrode_type_order
         self.neuron_type_order = neuron_type_order
         self.new_col_names = new_col_names
@@ -197,10 +244,51 @@ class NeuronTypesFigurePreprocessor:
         )
         return df
 
-    def preprocess_waveforms(self, probe_waveforms: pd.DataFrame) -> pd.DataFrame:
-        return self.waveform_preprocessor(probe_waveforms)
+    def preprocess_waveforms(
+        self, probe_waveforms: pd.DataFrame, df_probe_preprocessed: pd.DataFrame
+    ) -> Dict[str, pd.DataFrame]:
+        waveforms_pp = self.waveform_preprocessor(probe_waveforms).transpose()
+        # waveforms_pp.columns = waveforms_pp.columns.map(str)
 
-    def __call__(
+        out = {}
+        for neuron_type in self.neuron_type_order:
+            neurons = (
+                df_probe_preprocessed.loc[
+                    lambda x: x[self.neuron_type_col] == neuron_type
+                ][self.id_col]
+                .unique()
+                .tolist()
+                # .astype(str)
+            )
+            out[neuron_type] = waveforms_pp.loc[:, neurons]
+        return out
+
+    def preprocess_spikes(
+        self, probe_spikes: pd.DataFrame, df_probe_preprocessed: pd.DataFrame
+    ) -> Dict[str, List[np.ndarray]]:
+
+        out = {}
+        for neuron_type in self.neuron_type_order:
+            neurons = (
+                df_probe_preprocessed.loc[
+                    lambda x: x[self.neuron_type_col] == neuron_type
+                ][self.id_col]
+                .unique()
+                .tolist()
+            )
+            probe_spikes_sub = probe_spikes.loc[
+                lambda x: x[self.spikes_id_col].isin(neurons)
+            ]
+            trains = [
+                g[self.spikes_spiketime_col].values
+                for _, g in probe_spikes_sub.groupby(self.spikes_id_col)
+                if len(g[self.spikes_spiketime_col]) >= self.min_spikes_per_train
+                and len(g[self.spikes_spiketime_col]) <= self.max_spikes_per_train
+            ]
+            out[neuron_type] = trains
+        return out
+
+    def preprocess_all(
         self,
         probe_neuron_types: pd.DataFrame,
         probe_waveforms: pd.DataFrame,
@@ -208,7 +296,8 @@ class NeuronTypesFigurePreprocessor:
         probe_spiketrain_props: pd.DataFrame,
         single_unit_props: pd.DataFrame,
         single_unit_neuron_types: pd.DataFrame,
-    ):
+        spikes: pd.DataFrame,
+    ) -> Tuple[pd.DataFrame, pd.DataFrame, Dict[str, List[np.ndarray]]]:
         df_probe = self.preprocess_probe(
             probe_spiketrain_props,
             probe_waveform_props,
@@ -219,8 +308,9 @@ class NeuronTypesFigurePreprocessor:
             single_unit_neuron_types,
         )
         df = pd.concat([df_probe, df_single_unit]).reset_index(drop=True)
-        df_waveforms = self.preprocess_waveforms(probe_waveforms)
-        return df, df_waveforms
+        df_waveforms = self.preprocess_waveforms(probe_waveforms, df_probe)
+        trains = self.preprocess_spikes(spikes, df_probe)
+        return df, df_waveforms, trains
 
 
 class NeuronPropsPlotter:
@@ -292,72 +382,143 @@ class NeuronPropsPlotter:
 
 
 class NeuronTypesFigureGridMaker:
-    def __init__(
+    def make_figure(
         self,
         figsize: Tuple[float, float] = (7, 7),
         constrained_layout: bool = True,
-        top_bottom_hspace: float = 0.3,
-        top_bottom_height_ratios: Tuple[float, float] = (0.5, 0.5),
-        raster_waveforms_width_ratios: Tuple[float, float] = (0.9, 0.3),
-        raster_waveforms_wspace: float = 0.01,
-        bottom_figs_width_ratios: Tuple[float, float, float] = (0.5, 0.4, 0.2),
-        bottom_figs_wspace: float = 0.2,
-        raster_left: float = 0.05,
-        raster_right: float = 1,
-        raster_bottom: float = 0.1,
-        raster_top: float = 0.9,
-        raster_hspace: float = 0.1,
-        pct_top: float = 0.6,
-        pct_bottom: float = 0.3,
-        pct_left: float = 0.3,
-        pct_right: float = 0.8,
-        bar_wspave: float = 0.4,
-        bar_hspace: float = 0.4,
-        bar_top: float = 0.9,
-        bar_bottom: float = 0.1,
-        bar_right: float = 0.8,
+        **kwargs
     ):
-        self.figsize = figsize
-        self.constrained_layout = constrained_layout
-        self.top_bottom_hspace = top_bottom_hspace
-        self.top_bottom_height_ratios = top_bottom_height_ratios
-        self.raster_waveforms_width_ratios = raster_waveforms_width_ratios
-        self.raster_waveforms_wspace = raster_waveforms_wspace
-        self.raster_left = raster_left
-        self.raster_right = raster_right
-        self.raster_bottom = raster_bottom
-        self.raster_top = raster_top
-        self.raster_hspace = raster_hspace
-        self.pct_top = pct_top
-        self.pct_bottom = pct_bottom
-        self.pct_left = pct_left
-        self.pct_right = pct_right
-        self.bar_wspave = bar_wspave
-        self.bar_hspace = bar_hspace
-        self.bar_top = bar_top
-        self.bar_bottom = bar_bottom
-        self.bar_right = bar_right
-        self.bottom_figs_width_ratios = bottom_figs_width_ratios
-        self.bottom_figs_wspace = bottom_figs_wspace
+        f = plt.figure(figsize=figsize, constrained_layout=constrained_layout, **kwargs)
+        return f
 
-    def __call__(self):
-        out = {}
-        fig = plt.figure(
-            figsize=self.figsize, constrained_layout=self.constrained_layout
+    def split_top_bottom(self, fig: mpl.figure.Figure, hspace: float = 0.3, **kwargs):
+        fig_top, fig_bottom = fig.subfigures(nrows=2, hspace=hspace, **kwargs)
+        return fig_top, fig_bottom
+
+    def split_top_figs(
+        self,
+        fig: mpl.figure.Figure,
+        wspace: float = 0.01,
+        width_ratios=(0.9, 0.3),
+        **kwargs
+    ):
+        fig_raster, fig_waveforms = fig.subfigures(
+            ncols=2, wspace=wspace, width_ratios=width_ratios, **kwargs
         )
-        f_top, f_bottom = fig.subfigures(
-            nrows=2,
-            height_ratios=self.top_bottom_height_ratios,
-            hspace=self.top_bottom_hspace,
+        return fig_raster, fig_waveforms
+
+    def split_bottom_figs(
+        self,
+        fig: mpl.figure.Figure,
+        wspace: float = 0.2,
+        width_ratios=(0.5, 0.4, 0.2),
+        **kwargs
+    ):
+        fig_bottom_left, fig_bottom_middle, fig_bottom_right = fig.subfigures(
+            ncols=3, wspace=wspace, width_ratios=width_ratios, **kwargs
         )
-        f_raster, f_waveforms = f_top.subfigures(
-            ncols=2,
-            nrows=1,
-            width_ratios=self.raster_waveforms_width_ratios,
-            wspace=self.raster_waveforms_wspace,
+        return fig_bottom_left, fig_bottom_middle, fig_bottom_right
+
+
+class NeuronTypesPlotter:
+    def __init__(self):
+        ...
+
+    def plot_raster(
+        self,
+        trains: Dict[str, List[np.ndarray]],
+        fig: Optional[mpl.figure.Figure] = None,
+        grid_spec_kwargs: Optional[Dict[str, Any]] = None,
+        neuron_type_order: Optional[List[str]] = None,
+        height_ratios: Optional[List[float]] = None,
+        plot_kwargs: Optional[Dict[str, Any]] = None,
+        turn_off_axis: bool = True,
+    ):
+        fig = fig or plt.figure()
+        grid_spec_kwargs = grid_spec_kwargs or {}
+        grid_spec_kwargs["hspace"] = grid_spec_kwargs.get("hspace", 0.1)
+        grid_spec_kwargs["top"] = grid_spec_kwargs.get("top", 0.9)
+        grid_spec_kwargs["bottom"] = grid_spec_kwargs.get("bottom", 0)
+        grid_spec_kwargs["left"] = grid_spec_kwargs.get("left", 0.2)
+        grid_spec_kwargs["right"] = grid_spec_kwargs.get("right", 0.9)
+        print(grid_spec_kwargs)
+
+        plot_kwargs = plot_kwargs or {}
+        plot_kwargs["color"] = plot_kwargs.get("color", "black")
+        plot_kwargs["linewidths"] = plot_kwargs.get("linewidths", 1.5)
+        plot_kwargs["linelengths"] = plot_kwargs.get("linelengths", 1)
+        neuron_type_order = neuron_type_order or list(trains.keys())
+        if height_ratios is None:
+            height_ratios = [len(x) for x in trains.values()]
+        gs = fig.add_gridspec(
+            nrows=len(neuron_type_order),
+            ncols=1,
+            height_ratios=height_ratios,
+            **grid_spec_kwargs
         )
-        f_heat, f_bar, f_prop = f_bottom.subfigures(
-            ncols=3,
-            width_ratios=self.bottom_figs_width_ratios,
-            wspace=self.bottom_figs_wspace,
+        axes = {}
+        for neuron_type in neuron_type_order:
+            train_nt = trains[neuron_type]
+            axes[neuron_type] = fig.add_subplot(
+                gs[neuron_type_order.index(neuron_type), 0]
+            )
+            axes[neuron_type].eventplot(train_nt, **plot_kwargs)
+            if turn_off_axis:
+                axes[neuron_type].axis("off")
+
+    def plot_waveforms(
+        self,
+        waveforms: Dict[str, pd.DataFrame],
+        fig: Optional[mpl.figure.Figure] = None,
+        height_ratios: Optional[List[float]] = None,
+        grid_spec_kwargs: Optional[Dict[str, Any]] = None,
+        plot_kwargs: Optional[Dict[str, Any]] = None,
+        turn_off_axis: bool = True,
+        neuron_type_order: Optional[Sequence[str]] = None,
+        waveform_idx: int = 0,
+    ) -> Dict[str, mpl.axes.Axes]:
+        fig = fig or plt.figure()
+        grid_spec_kwargs = grid_spec_kwargs or {}
+        grid_spec_kwargs["top"] = grid_spec_kwargs.get("top", 0.7)
+        grid_spec_kwargs["bottom"] = grid_spec_kwargs.get("bottom", 0.1)
+        grid_spec_kwargs["left"] = grid_spec_kwargs.get("left", 0)
+        grid_spec_kwargs["right"] = grid_spec_kwargs.get("right", 0.7)
+        grid_spec_kwargs["wspace"] = grid_spec_kwargs.get("wspace", 0.7)
+        grid_spec_kwargs["hspace"] = grid_spec_kwargs.get("hspace", 0.7)
+
+        plot_kwargs = plot_kwargs or {}
+        plot_kwargs["color"] = plot_kwargs.get("color", "black")
+        plot_kwargs["linewidth"] = plot_kwargs.get("linewidth", 1.5)
+        neuron_type_order = neuron_type_order or list(waveforms.keys())
+        if height_ratios is None:
+            height_ratios = [1 for _ in range(len(neuron_type_order))]
+        gs = fig.add_gridspec(
+            nrows=len(neuron_type_order),
+            ncols=1,
+            height_ratios=height_ratios,
+            **grid_spec_kwargs
         )
+        axes = {}
+        for neuron_type in neuron_type_order:
+            waveform = waveforms[neuron_type].iloc[:, waveform_idx]
+            axes[neuron_type] = fig.add_subplot(
+                gs[neuron_type_order.index(neuron_type), 0]
+            )
+            axes[neuron_type].plot(waveform.index, waveform.values, **plot_kwargs)
+            if turn_off_axis:
+                axes[neuron_type].axis("off")
+        return axes
+
+    def plot_heatmaps(self, df_props: pd.DataFrame):
+        # silicon probe
+
+        ## pivot dropna and scale
+
+        ## heatmap
+
+        # glass pipette
+
+        ## pivot dropna and scale
+
+        ## heatmap
+        ...
