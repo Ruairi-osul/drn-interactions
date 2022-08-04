16b8701974b3b293393268ac1e312a38d3444f97
Ruairi O'Sullivan <ruairi.osullivan@univ.ox.ac.uk>
2022-08-04 17:18:08 -0400

    added seqnmf (#7)
    * added seqnmf to reqs
    
    * added seqnmf

--- drn_interactions/interactions/preprocessors.py
+++ drn_interactions/interactions/preprocessors.py
@@ -1,12 +1,20 @@
+from typing import Optional
 from scipy.stats import zscore
 from sklearn.preprocessing import minmax_scale
 from drn_interactions.transforms.brain_state_spikes import align_spikes_to_states_wide
+from scipy.ndimage import gaussian_filter1d
 
 
 class InteractionsPreprocessor:
-    def __init__(self, z: bool = True, minmax: bool = False):
+    def __init__(
+        self,
+        z: bool = True,
+        minmax: bool = False,
+        gaussian_sigma: Optional[float] = None,
+    ):
         self.z = z
         self.minmax = minmax
+        self.gaussian_sigma = gaussian_sigma
 
     def zscore(self, df):
         if self.z:
@@ -18,7 +26,13 @@ class InteractionsPreprocessor:
             return df.apply(minmax_scale)
         return df
 
+    def gaussian_filter(self, df):
+        if self.gaussian_sigma is not None:
+            return df.apply(lambda x: gaussian_filter1d(x, self.gaussian_sigma))
+        return df
+
     def __call__(self, df_piv):
         df_piv = self.zscore(df_piv)
         df_piv = self.min_max(df_piv)
+        df_piv = self.gaussian_filter(df_piv)
         return df_piv
