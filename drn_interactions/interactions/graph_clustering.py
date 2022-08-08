4c374e24c5547e00b326eb1162b0859521adb8b8
Ruairi O'Sullivan <ruairi.osullivan@univ.ox.ac.uk>
2022-08-08 10:54:00 -0400

    Added graph clustering descriptor (#15)
    * fixed bug in cluster evaluation
    
    * updated graph clustering for true stats
    
    * updated spont ensembles results

--- drn_interactions/interactions/graph_clustering.py
+++ drn_interactions/interactions/graph_clustering.py
@@ -10,6 +10,7 @@ from .pairwise import PairwiseMetric
 from .loaders import InteractionsLoader
 from .preprocessors import InteractionsPreprocessor
 from drn_interactions.stats import p_adjust
+from scipy.stats import entropy
 
 
 class SpectralCluster:
@@ -89,7 +90,8 @@ class ClusterEvaluation:
     def _partition_from_labels(df, labels):
         nodes = df.columns.values
         partition = [nodes[labels == i] for i in set(labels)]
-        return partition
+        names = list(set(labels))
+        return names, partition
 
     @staticmethod
     def _ms(G):
@@ -102,7 +104,7 @@ class ClusterEvaluation:
 
     def modularity(self, df, labels) -> float:
         G = self._make_graph(df)
-        partition = self._partition_from_labels(df, labels)
+        _, partition = self._partition_from_labels(df, labels)
         modularity = nx.algorithms.community.modularity(G, partition, weight="weight")
         return modularity
 
@@ -142,20 +144,29 @@ class ClusterEvaluation:
     def conductance(self, df, com) -> float:
         """(cut size) / ( min( {volume(not com), volume(com) }))"""
         G = self._make_graph(df)
-        conductance = nx.conductance(G, S=com, weight="weight")
-        return conductance
+        try:
+            conductance = nx.conductance(G, S=com, weight="weight")
+            return conductance
+        except ZeroDivisionError:
+            return np.nan
 
     def edge_expantion(self, df, com) -> float:
         """(cut size) / min( { size(not com), size(com) } )"""
         G = self._make_graph(df)
-        edge_expantion = nx.edge_expansion(G, S=com, weight="weight")
-        return edge_expantion
+        try:
+            edge_expantion = nx.edge_expansion(G, S=com, weight="weight")
+            return edge_expantion
+        except ZeroDivisionError:
+            return np.nan
 
     def normalized_cut_size(self, df, com) -> float:
         """[ 1 / (volume(com) / volume(not com))] * cut_size(com)"""
         G = self._make_graph(df)
-        normalized_cut_size = nx.normalized_cut_size(G, S=com, weight="weight")
-        return normalized_cut_size
+        try:
+            normalized_cut_size = nx.normalized_cut_size(G, S=com, weight="weight")
+            return normalized_cut_size
+        except ZeroDivisionError:
+            return np.nan
 
     def average_weight(self, df, com) -> float:
         G = self._make_graph(df)
@@ -164,8 +175,8 @@ class ClusterEvaluation:
         return np.mean(weights)
 
     def _apply_each_com(self, df, labels, f):
-        partition = self._partition_from_labels(df, labels)
-        return [f(df, com) for com in partition]
+        names, partition = self._partition_from_labels(df, labels)
+        return {name: f(df, com) for name, com in zip(names, partition)}
 
     def evaluate_partition(self, df, labels):
         modularity = self.modularity(df, labels)
@@ -180,9 +191,9 @@ class ClusterEvaluation:
         df,
         labels,
     ):
-        partition = self._partition_from_labels(df, labels)
+        names, partition = self._partition_from_labels(df, labels)
         results = []
-        for i, com in enumerate(partition):
+        for name, com in zip(names, partition):
             average_degree = self.average_degree(df, com)
             size = len(com)
             average_weight = self.average_weight(df, com)
@@ -193,7 +204,7 @@ class ClusterEvaluation:
             edge_expantion = self.edge_expantion(df, com)
             results.append(
                 (
-                    i,
+                    name,
                     size,
                     average_degree,
                     average_weight,
@@ -375,3 +386,151 @@ class ClusterRunner:
             df_affinity, ensembles_obs, partition_stats, ensemble_stats
         )
         return (df_affinity, ensembles, partition_stats, ensemble_stats)
+
+
+class ClusterResults:
+    def __init__(
+        self,
+        neuron_types: pd.DataFrame,
+        neuron_types_session_col: str = "session_name",
+        neuron_types_id_col: str = "neuron_id",
+        neuron_types_type_col: str = "neuron_type",
+        res_session_col: str = "session_name",
+        ensemble_id_col: str = "ensemble_id",
+        res_id_col: str = "neuron_id",
+    ):
+        self.neuron_types = neuron_types
+        self.neuron_types_session_col = neuron_types_session_col
+        self.neuron_types_id_col = neuron_types_id_col
+        self.neuron_types_type_col = neuron_types_type_col
+        self.res_session_col = res_session_col
+        self.res_id_col = res_id_col
+        self.ensemble_id_col = ensemble_id_col
+
+    def partition_results(self, ensemble_res: pd.DataFrame) -> pd.Series:
+        neuron_types = self.neuron_types[
+            [self.neuron_types_id_col, self.neuron_types_type_col]
+        ]
+        ensemble_res = ensemble_res.merge(
+            neuron_types, left_on=self.res_id_col, right_on=self.neuron_types_id_col
+        )
+        ensemble_res["in_ensemble"] = np.where(
+            ensemble_res[self.ensemble_id_col] == -1, False, True
+        )
+        out = {}
+        out["n"] = len(ensemble_res)
+        out["prop_all"] = ensemble_res["in_ensemble"].mean()
+        if self.neuron_types_type_col in ensemble_res.columns:
+            neuron_types = ensemble_res[self.neuron_types_type_col].unique()
+            for nt in neuron_types:
+                out[f"prop_{nt}"] = ensemble_res[
+                    ensemble_res[self.neuron_types_type_col] == nt
+                ]["in_ensemble"].mean()
+        return pd.Series(out)
+
+    def partition_results_by_session(self, ensemble_res: pd.DataFrame) -> pd.DataFrame:
+        """Extract results from clustering
+
+        - Proportion units in ensemble
+        - Proportion SR units in ensemble
+        - Proportion SIR units in ensemble
+        - Proportion FF units in ensemble
+        """
+        res_grouped = ensemble_res.groupby(self.res_session_col).apply(
+            self.partition_results
+        )
+        return res_grouped.reset_index().pivot(
+            index=self.res_session_col, columns="level_1", values=0
+        )
+
+    def ensemble_result(
+        self,
+        ensemble_id: Any,
+        ensemble_res: pd.DataFrame,
+        ensemble_stats: pd.DataFrame,
+        entropy_base: int = 2,
+    ) -> pd.Series:
+        session = ensemble_res.query(f"{self.ensemble_id_col} == {ensemble_id}")[
+            self.res_session_col
+        ].unique()[0]
+        neurons = self.neuron_types.query(
+            f"{self.neuron_types_session_col} == '{session}'"
+        )
+        df_session = ensemble_res.query(f"{self.res_session_col} == @session").merge(
+            neurons, left_on=self.res_id_col, right_on=self.neuron_types_id_col
+        )
+        df_ensemble = df_session.query(f"{self.ensemble_id_col} == {ensemble_id}")
+
+        # num neuros
+        session_num_neurons = df_session[self.res_id_col].nunique()
+        ensemble_num_neurons = df_ensemble[self.res_id_col].nunique()
+        # num neurons by type
+        session_num_units_by_type = (
+            df_session[self.neuron_types_type_col].value_counts().to_dict()
+        )
+        ensemble_num_units_by_type = (
+            df_ensemble[self.neuron_types_type_col].value_counts().to_dict()
+        )
+
+        out = ensemble_stats.query(f"{self.ensemble_id_col} == {ensemble_id}").iloc[
+            0, :
+        ]
+        out["prop_total"] = ensemble_num_neurons / session_num_neurons
+        for nt in df_ensemble[self.neuron_types_type_col].unique():
+            out[f"prop_total_{nt}"] = (
+                ensemble_num_units_by_type[nt] / session_num_units_by_type[nt]
+            )
+            out[f"comp_{nt}"] = ensemble_num_units_by_type[nt] / ensemble_num_neurons
+
+        out["neuron_type_entropy"] = self._entropy(
+            df_ensemble[self.neuron_types_type_col], base=entropy_base
+        )
+        return out
+
+    @staticmethod
+    def _entropy(ser, base=2, qk=None):
+        value_counts = ser.value_counts().values
+        return entropy(value_counts, base=base, qk=qk)
+
+    def ensemble_response_entropy(
+        self,
+        ensemble_res: pd.DataFrame,
+        df_response: pd.DataFrame,
+        df_response_id_col: str = "neuron_id",
+        df_response_response_col: str = "response",
+        created_col: str = "response_entropy",
+        base: int = 2,
+    ) -> pd.DataFrame:
+
+        ensemble_res = ensemble_res.merge(
+            df_response[[df_response_id_col, df_response_response_col]],
+            left_on=self.res_id_col,
+            right_on=df_response_id_col,
+        )
+        res = (
+            ensemble_res.groupby(self.ensemble_id_col)[df_response_response_col]
+            .apply(self._entropy, base=base)
+            .reset_index()
+            .rename(columns={df_response_response_col: created_col})
+        )
+        return res
+
+    def ensemble_results(
+        self, ensemble_res: pd.DataFrame, ensemble_stats: pd.DataFrame
+    ) -> pd.DataFrame:
+        """Extract results from ensemble
+
+        - Size
+        - Normalized Size
+        - Edge Expansion
+        - Conductivity
+        - Proportion SR
+        - Proportion SIR
+        - Proportion FF
+        """
+        ensembles = ensemble_res[self.ensemble_id_col].unique()
+        ser_list = []
+        for id in ensembles:
+            ser_list.append(self.ensemble_result(id, ensemble_res, ensemble_stats))
+
+        return pd.DataFrame(ser_list)
