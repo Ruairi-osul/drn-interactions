ba17c09d0b26e6a59a45828aabebcfbd91b3c07b
Ruairi-osul <ruairi.osullivan@univ.ox.ac.uk>
2022-08-24 10:43:33 -0400

    Added section of preprocessor deal inactivity

--- drn_interactions/interactions/preprocessors.py
+++ drn_interactions/interactions/preprocessors.py
@@ -11,10 +11,14 @@ class InteractionsPreprocessor:
         z: bool = True,
         minmax: bool = False,
         gaussian_sigma: Optional[float] = None,
+        col_thresh: float = 0,
+        row_thresh: float = 0,
     ):
         self.z = z
         self.minmax = minmax
         self.gaussian_sigma = gaussian_sigma
+        self.col_thresh = col_thresh
+        self.row_thresh = row_thresh
 
     def zscore(self, df):
         if self.z:
@@ -26,13 +30,22 @@ class InteractionsPreprocessor:
             return df.apply(minmax_scale)
         return df
 
+    def exclude_cols(self, df):
+        return df.loc[:, (df.sum(axis=0) != self.col_thresh)]
+
+    def exclude_rows(self, df):
+        return df.loc[(df.sum(axis=1) != self.row_thresh), :]
+
     def gaussian_filter(self, df):
         if self.gaussian_sigma is not None:
             return df.apply(lambda x: gaussian_filter1d(x, self.gaussian_sigma))
         return df
 
     def __call__(self, df_piv):
+        df_piv = self.exclude_cols(df_piv)
+        df_piv = self.exclude_rows(df_piv)
         df_piv = self.zscore(df_piv)
         df_piv = self.min_max(df_piv)
         df_piv = self.gaussian_filter(df_piv)
-        return df_piv
+
+        return df_piv.copy()
